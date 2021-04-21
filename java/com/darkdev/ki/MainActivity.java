0a8aebb2a368fe860f8f172b3b89fd0ec5f8f805
Dark Tornado <dt3141592@naver.com>
2021-04-21 22:53:49 +0900

    Add files via upload

---
+++ java/com/darkdev/ki/MainActivity.java
@@ -0,0 +1,13 @@
+package com.darkdev.ki;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
