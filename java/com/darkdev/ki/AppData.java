876ddb948c037433957ecfb7ba0e91ae56c0575c
Dark Tornado <dt3141592@naver.com>
2021-05-05 12:46:03 +0900

    Add files via upload

---
+++ java/com/darkdev/ki/AppData.java
@@ -0,0 +1,14 @@
+package com.darkdev.ki;
+
+import android.graphics.drawable.Drawable;
+
+public class AppData {
+    public String name;
+    public String pack;
+    public Drawable icon;
+
+    public AppData(String name, String pack) {
+        this.name = name;
+        this.pack = pack;
+    }
+}
