99165f31245bb2dd17e3a604ba5b39a8de213fb0
Dark Tornado <dt3141592@naver.com>
2021-05-13 23:07:12 +0900

    Add files via upload

---
+++ java/com/darkdev/ki/WeatherActivity.kt
@@ -0,0 +1,13 @@
+package com.darkdev.ki
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class WeatherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
