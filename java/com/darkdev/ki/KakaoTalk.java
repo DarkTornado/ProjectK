ca32b8743a80c17ff18478137da2df554cde9c6b
Dark Tornado <dt3141592@naver.com>
2021-06-07 19:46:03 +0900

    수신된 카카오톡 채팅 정보를 저장할 클래스

---
+++ java/com/darkdev/ki/KakaoTalk.java
@@ -0,0 +1,41 @@
+package com.darkdev.ki;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.RemoteInput;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+
+public class KakaoTalk {
+    private Context ctx;
+    private Notification.Action act;
+
+    String room, msg, sender;
+    boolean isGroupChat;
+
+    public KakaoTalk(Context ctx, Bundle data, Notification.Action act) {
+        this.ctx = ctx;
+        this.act = act;
+        sender = data.getString("android.title");
+        msg = data.getString("android.text");
+        room = data.getString(Build.VERSION.SDK_INT > 23 ? "android.summaryText" : "android.subText");
+        isGroupChat = room != null;
+        if (room == null) room = sender;
+    }
+
+
+    public void reply(String value) {
+        Intent sendIntent = new Intent();
+        Bundle msg = new Bundle();
+        for (RemoteInput input : act.getRemoteInputs()) {
+            msg.putCharSequence(input.getResultKey(), value);
+        }
+        RemoteInput.addResultsToIntent(act.getRemoteInputs(), sendIntent, msg);
+        try {
+            act.actionIntent.send(ctx, 0, sendIntent);
+        } catch (PendingIntent.CanceledException e) {
+        }
+    }
+}
