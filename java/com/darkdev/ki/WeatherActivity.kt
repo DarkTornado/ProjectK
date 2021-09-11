c50294df6b5a7e6e8c4c2166a3b9ac32cc791d16
Dark Tornado <dt3141592@naver.com>
2021-09-12 05:22:36 +0900

    날씨 기능 다듬기

--- java/com/darkdev/ki/WeatherActivity.kt
+++ java/com/darkdev/ki/WeatherActivity.kt
@@ -1,11 +1,5 @@
 package com.darkdev.ki
 
-import android.content.res.ColorStateList
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
-import android.graphics.drawable.Drawable
-import android.graphics.drawable.RippleDrawable
-import android.os.Build
 import android.os.Bundle
 import android.webkit.WebChromeClient
 import android.webkit.WebView
@@ -13,7 +7,6 @@ import android.webkit.WebViewClient
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.darkdev.uilib.BottomNavigationLayout
 
 class WeatherActivity : AppCompatActivity() {
 
@@ -21,7 +14,6 @@ class WeatherActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val data = intent.getStringExtra("data")
         val pos = intent.getStringExtra("pos")
-        val loc = intent.getStringExtra("loc")
         if (data == null) {
             toast("비정상적인 실행이 감지되었어요 :(")
             finish()
@@ -41,15 +33,6 @@ class WeatherActivity : AppCompatActivity() {
             web.layoutParams = LinearLayout.LayoutParams(-1, -1)
 
         setContentView(web)
-    }
-
-    fun getRipple(): Drawable? {
-        val color = Color.parseColor("#81D4FA")
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            RippleDrawable(ColorStateList.valueOf(Color.WHITE), ColorDrawable(color), null)
-        } else {
-            ColorDrawable(color)
-        }
     }
 
     fun toast(msg: String) = Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
